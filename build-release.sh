a5a66195a6854d86a7c66f05c6f2021eedcbbcf7
Detlef Stern <detlef.stern@hs-heilbronn.de>
2019-11-08 17:09:54 +0100

    Add versioning to build script

--- build-release.sh
+++ build-release.sh
@@ -2,9 +2,10 @@
 
 export GOARCH=amd64
 export LDFLAGS="-s -w"
+TAG_VERSION=$(git tag | sort | tail -n 1)
 for GOOS in "linux" "darwin" "windows"
 do
-  EXECUTABLE="go-auth-s-$GOOS-$GOARCH"
+  EXECUTABLE="go-auth-s-$TAG_VERSION-$GOOS-$GOARCH"
   GOOS="$GOOS" go build -o "$EXECUTABLE" -ldflags "$LDFLAGS"
   if [ "$GOOS" = "windows" ]; then
     mv "$EXECUTABLE" "$EXECUTABLE.exe"
